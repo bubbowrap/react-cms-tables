416f51efd5b984b55332264f2fa4620c34152fe5
Jeremy Samuel <pentium6.0@gmail.com>
2021-10-20 14:35:24 -0400

    removing unused vars

--- src/pages/ProviderSearch.js
+++ src/pages/ProviderSearch.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
-import classes from '../App.module.css';
-import axios from 'axios';
-import { useTable } from 'react-table';
-import '../components/UI/Table.module.css';
-import Button from '../components/UI/Button/Button';
-import Input from '../components/UI/Input/Input';
-import Select from '../components/UI/Select/Select';
+// import React, { useEffect, useRef, useState } from 'react';
+// import classes from '../App.module.css';
+// import axios from 'axios';
+// import { useTable } from 'react-table';
+// import '../components/UI/Table.module.css';
+// import Button from '../components/UI/Button/Button';
+// import Input from '../components/UI/Input/Input';
+// import Select from '../components/UI/Select/Select';
 
 const ProviderSearch = () => {
   return (
-    <div className={classes['l-container']}>
+    <div>
       <h1>Provider Search</h1>
       <p>Search by Provider.</p>
     </div>
